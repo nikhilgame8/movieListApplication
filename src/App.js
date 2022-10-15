9babf1bf1003901c3122c3b4909c5c0eb0245661
nikhilgame8 <nikhilking135@gmail.com>
2022-10-15 06:55:11 +0530

    Movie List using react js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Movies from "./component/Movies";
+import "./App.css";
 
 function App() {
+  const [theme, setTheme] = useState("white");
+  const [cardTheme, setCardTheme] = useState("#808080");
+
+  const changeTheme = () =>{
+    if(theme === "#454545"){
+      setTheme("white")
+    }else setTheme("#454545")
+    if(cardTheme === "#808080"){
+      setCardTheme("#B3B3B3")
+    }else setCardTheme("#808080")
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="dark">
+      <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <span className="navbar-brand mb-0 h1">Movie List</span>
+          <div class="form-check form-switch">
+          <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" onClick={changeTheme}/>
+          {theme==="white"?<label class="form-check-label text-white"  for="flexSwitchCheckDefault">Dark Mode</label> : 
+          <label class="form-check-label text-white" for="flexSwitchCheckDefault">Ligh Mode</label>}
+          </div>
+        </div>
+      </nav>
+      <Movies theme={theme} cardTheme={cardTheme}/>
     </div>
   );
 }
-
 export default App;
